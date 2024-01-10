d5145a4dd92e8268ae46624985ddf00f02778c42
0Zerotime <152350856+0Zerotime@users.noreply.github.com>
2024-01-10 14:49:47 +0800

    Add files via upload

---
+++ 龙族/上杉越/上杉越.js
@@ -0,0 +1,4 @@
+var btn1 = document.getElementById("btn");
+btn1.onclick = function () {
+  console.log("HelloDOM0级事件处理程序1");
+};
---
+++ 龙族/主页/homepage.js
@@ -0,0 +1,4 @@
+var btn1 = document.getElementById("btn");
+btn1.addEventListener("click", function() {
+  console.log("Hello DOM2级事件处理程序");
+});
